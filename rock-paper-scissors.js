95d13660afc8a12138076af9fd27f66ee2adf0d9
thesatmannn <seanom12@gmail.com>
2022-07-28 17:31:24 -0500

    loop 5 rounds and tally score

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,7 +1,7 @@
 //randomly return rock, paper and scissors
 
 function getComputerChoice() {
-    const randomChoice = Math.floor(Math.random() * 3) + 1;
+    let randomChoice = Math.floor(Math.random() * 3) + 1;
     if (randomChoice === 1) {
       return "Rock";
     }
@@ -12,22 +12,40 @@ function getComputerChoice() {
       return "Scissors";
     }
   }
+  // use computerchoice and user input to determine winner of game
+
   function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
       return "It's a tie!"
     }
       else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper" || playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors" || playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock" ) {
         console.log("You win! " + playerSelection + " beats " + computerSelection + ".")
+        playerScore++;
       }
       else {
         console.log("You lose! " + computerSelection + " beats " + playerSelection + ".")
-
+        computerScore++;
       }
   }
-  function game() {
-    
+  //keep track of rounds and tally score 
 
-  }
-  const playerSelection = "RocK";
-  const computerSelection = getComputerChoice();
- console.log(playRound(playerSelection, computerSelection));
+  function game() {
+    for (let i = 0; i < 5; i++) {
+      let playerSelection = prompt("Rock, Paper or Scissors?");
+      console.log(playRound(playerSelection, getComputerChoice()));
+      }
+       if ( i = 5 && playerScore > computerScore) {
+        console.log("You won the game!");
+      }
+      else if ( i = 5 && computerScore > playerScore) {
+        console.log("You lose!");
+      }
+      else {
+        console.log("it's a draw!");
+      }
+    }
+  let playerScore = 0;
+  let computerScore = 0;
+  let computerSelection = getComputerChoice();
+  console.log(game());
+  
